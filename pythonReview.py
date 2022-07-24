77c0f122f79e73c1af1fc0623028d8af69474a42
roaaab23-meet <roaa.ab86@gmail.com>
2022-07-24 18:45:00 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,19 @@
+def create_youtube_video(title, description,):
+	youtubevideo={"title":title, "description":description,"likes":0, "dislikes":0, "comments"={}}
+	return dict1
+
+def like(youtube_video):
+	if "like" in youtube_video:
+		youtubevideo["like"]+=1;
+	return create_youtube_video
+def dislike(youtube_video):
+	if "dislike" in youtube_video:
+		youtubevideo["dislike"]+=1;
+	return create_youtube_video
+def add_comment():
+	youtubevideo["comments"][username]=comment_text
+	return youtubevideo
+
+
+
+

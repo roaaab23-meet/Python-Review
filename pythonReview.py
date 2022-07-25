c790c37d57e57934a5321d1c249df6044e9f609d
roaaab23-meet <roaa.ab86@gmail.com>
2022-07-25 16:42:22 +0300

    python review

--- pythonReview.py
+++ pythonReview.py
@@ -1,19 +1,25 @@
 def create_youtube_video(title, description,):
-	youtubevideo={"title":title, "description":description,"likes":0, "dislikes":0, "comments"={}}
-	return dict1
-
-def like(youtube_video):
-	if "like" in youtube_video:
-		youtubevideo["like"]+=1;
-	return create_youtube_video
-def dislike(youtube_video):
-	if "dislike" in youtube_video:
-		youtubevideo["dislike"]+=1;
-	return create_youtube_video
-def add_comment():
-	youtubevideo["comments"][username]=comment_text
+	youtubevideo={"title":title, "description":description,"likes":0, "dislikes":0, "comments":{}}
 	return youtubevideo
 
+def like(youtubevideo):
+	if "likes" in youtubevideo:
+		youtubevideo["likes"]+=1
+	return create_youtube_video
+def dislike(youtubevideo):
+	if "dislikes" in youtubevideo:
+		youtubevideo["dislikes"]+=1
+	return create_youtube_video
+def add_comment(comment_text,youtubevideo,username):
+	youtubevideo["comments"][username]=comment_text
+	return youtubevideo
+x=create_youtube_video("thisistitle", "thisisdesc")
+like(x)
+dislike(x)
+add_comment("hi", x,"hello")
+for i in range(1,495):
+	like(x)
+print(x)
 
 
 
